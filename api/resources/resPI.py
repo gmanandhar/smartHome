21ff3ae82475374f8d8374e9e388b30a3dbe7a5b
gaurabmanandhar <mdr.gaurab@gmail.com>
2020-03-07 17:03:24 +1100

    Add Configuration of Respeberry Pi

--- api/resources/resPI.py
+++ api/resources/resPI.py
@@ -1,13 +1,16 @@
 from api.models import pi
 
 def resPi(pinIn, pinOut, status):
+    sts = 1
+    if status== 1:
+        sts = 0
     pi.setup(pinIn, pi.IN)
     pi.setup(pinOut, pi.OUT)
     # Input from pin 11
     pinStatus = pi.input(pinIn)
-    if pinStatus:
-        pi.setup(pinOut, status)
+    if not pinStatus:
+        pi.setup(pinOut, sts)
         return False
     else:
-        pi.setup(pinOut, status)
+        pi.setup(pinOut, sts)
         return True
