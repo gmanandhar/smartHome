f27dfc6ae22f060247acfaffccd98fe61d6de144
gaurabmanandhar <mdr.gaurab@gmail.com>
2020-04-10 09:20:33 +1000

    Change in Task

--- api/resources/task.py
+++ api/resources/task.py
@@ -1,10 +1,11 @@
 import logging as logger
-import datetime, pandas as pd, time
+import datetime, time
 
 def add_task(futureDate):
     logger.debug("Task Added into Queue")
     current_date = datetime.datetime.now()
-    future_date = pd.to_datetime(futureDate)
+    # future_date = pd.to_datetime(futureDate)
+    future_date = current_date + datetime.timedelta(minutes = futureDate)
     duration = (future_date-current_date).total_seconds()
     when_to_stop = abs(int(duration))
     while when_to_stop >0:
